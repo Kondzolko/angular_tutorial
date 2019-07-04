d1079263b19739ba3fb2e5bc035b2c2d42ba1fef
andriy.kondzolko <kondzolko@gmail.com>
2019-07-04 17:54:09 -0400

    Added documents, link and data!

--- app/documents/documents.component.ts
+++ app/documents/documents.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { Document } from "./document"
 
 @Component({
     moduleId: module.id,
@@ -6,5 +7,28 @@ import { Component } from "@angular/core";
     templateUrl: 'documents.component.html'
 })
 export class DocumentsComponent {
+    documents: Document[] = [
+        {
+            title: 'My First Doc',
+            description: 'taefasf asf asf asf asfa s f',
+            file_url: 'http://google.com',
+            updated_at: '11/11/2019',
+            image_url: 'http://google.com'
+        },
+        {
+            title: 'My Second Doc',
+            description: 'taefasf asf asf asf asfa s f',
+            file_url: 'http://google.com',
+            updated_at: '11/11/2019',
+            image_url: 'http://google.com'
+        },
+        {
+            title: 'My Last Doc',
+            description: 'taefasf asf asf asf asfa s f',
+            file_url: 'http://google.com',
+            updated_at: '11/11/2019',
+            image_url: 'http://google.com'
+        },
 
+    ]
 }
